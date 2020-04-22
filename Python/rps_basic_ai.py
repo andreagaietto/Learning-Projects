7639bec50b921fe02f7cb740c0585b664c3ffa3f
agaietto <a.gaietto1@gmail.com>
2020-04-22 17:01:11 -0400

    Create new collection for Python learning projects.

---
+++ Python/rps_basic_ai.py
@@ -0,0 +1,34 @@
+from random import randint
+print("Rock...")
+print("Paper...")
+print("Scissors...")
+
+player = input("Make your move: ").lower()
+rand_num = randint(1, 3)
+
+if rand_num == 1:
+    computer = "rock"
+elif rand_num == 2:
+    computer = "paper"
+else:
+     computer = "scissors"
+
+if player == computer:
+    print("It's a tie, computer chose " + computer + "!")
+elif player == "rock":
+	if computer == "paper":
+		print("Player 2 wins, computer chose " + computer + "!")
+	else:
+		print("Player 1 wins, computer chose " + computer + "!")
+elif player == "paper":
+	if computer == "rock":
+		print("Player 1 wins, computer chose " + computer + "!")
+	else:
+		print("Player 1 wins, computer chose " + computer + "!")
+elif player == "scissors":
+	if computer == "rock":
+		print("Player 2 wins, computer chose " + computer + "!")
+	else:
+		print("Player 1 wins, computer chose " + computer + "!")
+else:
+	print("Please enter a valid move.")
