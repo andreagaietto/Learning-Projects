e920fd85e77568156689c1470785bdd336844a3a
agaietto <a.gaietto1@gmail.com>
2020-08-12 09:18:41 -0400

    final decorator problems

---
+++ Python/Small exercises/decorators/delay.py
@@ -0,0 +1,25 @@
+from functools import wraps
+from time import sleep
+
+def delay(val):
+    def inner(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            print(f"Waiting {val}s before running say_hi")
+            sleep(val)
+            return fn(*args, **kwargs)
+        return wrapper
+    return inner
+
+
+
+
+@delay(3)
+def say_hi():
+    return "hi"
+
+
+print(say_hi())
+# should print the message "Waiting 3s before running say_hi" to the console
+# should then wait 3 seconds
+# finally, should invoke say_hi and return "hi"
