0dc4a74335d8c90042aa4f53720c8d6f294fefc1
Andrea Gaietto <a.gaietto1@gmail.com>
2020-10-11 16:49:54 -0400

    java and js coursework

---
+++ JS/Chapter 6/classValidate.js
@@ -0,0 +1,11 @@
+function result() {
+    var TestVar = document.getElementById("data").value;
+    var testRegEx = /[A-Z]{3}\.\d{3}#\d{4}_[a-z]{2}-\d{4}/;
+
+    if (testRegEx.test(TestVar)) {
+        document.getElementById("feedback").innerHTML = "Correct Format";
+    }
+    else {
+        document.getElementById("feedback").innerHTML = "Incorrect Format";
+    }
+}
